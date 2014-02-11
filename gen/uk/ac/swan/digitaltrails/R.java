33ce4305b9ff2ec17bc1fe093c9befac4561a40a
LJHancock <lewisjhancock@gmail.com>
2014-02-11 17:25:41 +0000

    google maps needs library

--- gen/uk/ac/swan/digitaltrails/R.java
+++ gen/uk/ac/swan/digitaltrails/R.java
@@ -94,25 +94,27 @@ public final class R {
         public static final int wr008a=0x7f020050;
     }
     public static final class id {
-        public static final int address=0x7f050003;
-        public static final int description=0x7f050002;
-        public static final int image=0x7f050000;
-        public static final int imageView1=0x7f050005;
-        public static final int listView1=0x7f050009;
-        public static final int mapview=0x7f05000a;
-        public static final int scrollView1=0x7f050006;
-        public static final int tab1Layout=0x7f050008;
-        public static final int textView1=0x7f050004;
-        public static final int textView2=0x7f050007;
-        public static final int title=0x7f050001;
+        public static final int address=0x7f050004;
+        public static final int description=0x7f050003;
+        public static final int image=0x7f050001;
+        public static final int imageView1=0x7f050006;
+        public static final int listView1=0x7f05000a;
+        public static final int map=0x7f050000;
+        public static final int mapview=0x7f05000b;
+        public static final int scrollView1=0x7f050007;
+        public static final int tab1Layout=0x7f050009;
+        public static final int textView1=0x7f050005;
+        public static final int textView2=0x7f050008;
+        public static final int title=0x7f050002;
     }
     public static final class layout {
-        public static final int bonuspack_bubble=0x7f030000;
-        public static final int info=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int tab1=0x7f030003;
-        public static final int tab2=0x7f030004;
-        public static final int walkmap=0x7f030005;
+        public static final int activity_main=0x7f030000;
+        public static final int bonuspack_bubble=0x7f030001;
+        public static final int info=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int tab1=0x7f030004;
+        public static final int tab2=0x7f030005;
+        public static final int walkmap=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- src/uk/ac/swan/digitaltrails/MainActivity.java
+++ src/uk/ac/swan/digitaltrails/MainActivity.java
@@ -1,37 +1,51 @@
 package uk.ac.swan.digitaltrails;
 
-import uk.ac.swan.digitaltrails.R;
-
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.Menu;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity {
+  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+  static final LatLng KIEL = new LatLng(53.551, 9.993);
+  private GoogleMap map;
 
-	/** Called when the activity is first created. */
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+@Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.tab1);
-		ImageView imgView = (ImageView) findViewById(R.id.imageView1);
-		TextView titleText = (TextView) findViewById(R.id.textView1);
-		TextView introText = (TextView) findViewById(R.id.textView2);
+    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+        .getMap();
+    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+        .title("Hamburg"));
+    Marker kiel = map.addMarker(new MarkerOptions()
+        .position(KIEL)
+        .title("Kiel")
+        .snippet("Kiel is cool")
+        .icon(BitmapDescriptorFactory
+            .fromResource(R.drawable.ic_launcher)));
 
-		// set app customization here
-		imgView.setImageDrawable(this.getResources().getDrawable(
-				R.drawable.abershore));
+    // Move the camera instantly to hamburg with a zoom of 15.
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
 
-		titleText.setText("ABERYSTWYTH TOWN WALKS");
-		introText
-				.setText("Welcome to Aberystwyth Town Walks for Android. Please select a walk of your choice from the 'Walks' tab to start exploring some of the places of interest within Aberystwyth town.");
+    // Zoom in, animating the camera.
+    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+  }
 
-	}
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    //getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
 
-	public void onClick(View src) {
-
-	}
-}
+} 
